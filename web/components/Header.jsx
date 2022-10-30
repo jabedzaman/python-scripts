53bbc89e28b953792808d457b3e2ef7860117fc6
jabed <jabedzaman2004@gmail.com>
2022-10-31 03:04:32 +0530

    add logo

--- web/components/Header.jsx
+++ web/components/Header.jsx
@@ -5,20 +5,22 @@ function Header() {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mt-0 mx-auto px-4 sm:px-6">
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+        <motion.div
+        initial={{
+          opacity: 0,
+          y: -100,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 0.2,
+        }}
+        className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <motion.a
-            initial={{
-              opacity: 0,
-              x: -100
-            }}
-            whileInView={{
-              opacity: 1,
-              x: 0
-            }}
-            transition={{
-              duration: 0.5
-            }}
+            <a
             href="#">
               <span className="sr-only">Jabed Zaman</span>
               {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -27,27 +29,15 @@ function Header() {
                 src="/palm-tree_1f334.png"
                 alt=""
               />
-            </motion.a>
+            </a>
           </div>
-          <motion.div
-          initial={{
-            opacity: 0,
-            x: 100
-          }}
-          whileInView={{
-            opacity: 1,
-            x: 0
-          }}
-          transition={{
-            duration: 0.5
-          }}
-          >
+          <div>
             <a
             href="https://xenseee.tech" className="italic text-xs lg:text-2xl ">
             Created by <span className="text-blue-500 font-semibold">Jabed</span>
             </a>
-          </motion.div>
-        </div>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
