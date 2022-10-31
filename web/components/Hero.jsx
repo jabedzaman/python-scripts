ef76e44b2692fda68cae994af4fd1574d0ebc015
jabed <jabedzaman2004@gmail.com>
2022-11-01 02:52:06 +0530

    fix: github button

--- web/components/Hero.jsx
+++ web/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import GitHubButton from "react-github-btn";
 import { motion } from "framer-motion";
-import {useTheme} from 'next-themes'
+import { useTheme } from "next-themes";
 
 function Hero() {
-  const { theme } = useTheme()
+  const { theme } = useTheme();
   return (
-    <div>
+    <div className="">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <a href="https://github.com/jabedzaman/python-scripts">
         <motion.img
@@ -25,91 +25,99 @@ function Hero() {
           transition={{
             duration: 0.5,
           }}
-          src={theme === 'dark' ? 'https://ik.imagekit.io/jabedzaman/Python_Scripts/Python_Scripts__2__ZeFyxDnqp.png?ik-sdk-version=javascript-1.4.3&updatedAt=1667247222014' : "https://ik.imagekit.io/jabedzaman/Python_Scripts/Python_Scripts_Wez3Xcy23.png?ik-sdk-version=javascript-1.4.3&updatedAt=1667035157365"}
+          src={
+            theme === "dark"
+              ? "https://ik.imagekit.io/jabedzaman/Python_Scripts/Python_Scripts__2__ZeFyxDnqp.png?ik-sdk-version=javascript-1.4.3&updatedAt=1667247222014"
+              : "https://ik.imagekit.io/jabedzaman/Python_Scripts/Python_Scripts_Wez3Xcy23.png?ik-sdk-version=javascript-1.4.3&updatedAt=1667035157365"
+          }
           alt="Python-Scripts"
         />
       </a>
       <motion.div
-      initial={{
-        opacity: 0,
-        y: 100,
-      }}
-      whileInView={{
-        opacity: 1,
-        y: 0,
-      }}
-      exit={{
-        opacity: 0,
-        y: 100,
-      }}
-      transition={{
-        duration: 0.5,
-      }}
-      className="flex flex-row max-w-2xl justify-evenly mx-auto">
-      {/* <!-- Place this tag where you want the button to render. --> */}
-      <GitHubButton
-        href="https://github.com/jabedzaman"
-        data-color-scheme="no-preference: light; light: light; dark: light;"
-        data-size="large"
-        aria-label="Follow @jabedzaman on GitHub"
+        initial={{
+          opacity: 0,
+          y: 100,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        exit={{
+          opacity: 0,
+          y: 100,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        className="flex mt-4 flex-col max-w-2xl justify-center items-center space-y-2 mx-auto"
       >
-        Follow @jabedzaman
-      </GitHubButton>
-      <GitHubButton
-        href="https://github.com/HOSENUR"
-        data-color-scheme="no-preference: light; light: light; dark: light;"
-        data-size="large"
-        aria-label="Follow @HOSENUR on GitHub"
-      >
-        Follow @HOSENUR
-      </GitHubButton>
-      {/* <!-- Place this tag where you want the button to render. --> */}
-      <GitHubButton
-        href="https://github.com/jabedzaman/python-scripts"
-        data-color-scheme="no-preference: light; light: light; dark: light;"
-        data-icon="octicon-star"
-        data-size="large"
-        aria-label="Star jabedzaman/python-scripts on GitHub"
-      >
-        Star
-      </GitHubButton>
-      {/* <!-- Place this tag where you want the button to render. --> */}
-      <section className="hidden md:block">
-
-      <GitHubButton
-        href="https://github.com/jabedzaman/python-scripts/fork"
-        data-color-scheme="no-preference: light; light: light; dark: light;"
-        data-icon="octicon-repo-forked"
-        data-size="large"
-        aria-label="Fork jabedzaman/python-scripts on GitHub"
-        >
-        Fork
-      </GitHubButton>
-        </section>
-      <div className="hidden lg:inline">
-      {/* <!-- Place this tag where you want the button to render. --> */}
-      <GitHubButton
-        href="https://github.com/jabedzaman/python-scripts/subscription"
-        data-color-scheme="no-preference: light; light: light; dark: light;"
-        data-icon="octicon-eye"
-        data-size="large"
-        aria-label="Watch jabedzaman/python-scripts on GitHub"
-      >
-        Watch
-      </GitHubButton>
-      </div>
-      {/* <!-- Place this tag where you want the button to render. --> */}
-      <div className="hidden lg:inline">
-      <GitHubButton
-        href="https://github.com/jabedzaman/python-scripts/discussions"
-        data-color-scheme="no-preference: light; light: light; dark: light;"
-        data-icon="octicon-comment-discussion"
-        data-size="large"
-        aria-label="Discuss jabedzaman/python-scripts on GitHub"
-      >
-        Discuss
-      </GitHubButton>
-      </div>
+        <div className="flex flex-row  space-x-2">
+          {/* <!-- Place this tag where you want the button to render. --> */}
+          <GitHubButton
+            href="https://github.com/jabedzaman"
+            data-color-scheme="no-preference: light; light: light; dark: light;"
+            data-size="large"
+            aria-label="Follow @jabedzaman on GitHub"
+          >
+            Follow @jabedzaman
+          </GitHubButton>
+          <GitHubButton
+            href="https://github.com/HOSENUR"
+            data-color-scheme="no-preference: light; light: light; dark: light;"
+            data-size="large"
+            aria-label="Follow @HOSENUR on GitHub"
+          >
+            Follow @HOSENUR
+          </GitHubButton>
+        </div>
+        <div className="flex flex-row  space-x-2">
+          {/* <!-- Place this tag where you want the button to render. --> */}
+          <GitHubButton
+            href="https://github.com/jabedzaman/python-scripts"
+            data-color-scheme="no-preference: light; light: light; dark: light;"
+            data-icon="octicon-star"
+            data-size="large"
+            aria-label="Star jabedzaman/python-scripts on GitHub"
+          >
+            Star
+          </GitHubButton>
+          {/* <!-- Place this tag where you want the button to render. --> */}
+          <section className="">
+            <GitHubButton
+              href="https://github.com/jabedzaman/python-scripts/fork"
+              data-color-scheme="no-preference: light; light: light; dark: light;"
+              data-icon="octicon-repo-forked"
+              data-size="large"
+              aria-label="Fork jabedzaman/python-scripts on GitHub"
+            >
+              Fork
+            </GitHubButton>
+          </section>
+          <div className="hidden lg:inline">
+            {/* <!-- Place this tag where you want the button to render. --> */}
+            <GitHubButton
+              href="https://github.com/jabedzaman/python-scripts/subscription"
+              data-color-scheme="no-preference: light; light: light; dark: light;"
+              data-icon="octicon-eye"
+              data-size="large"
+              aria-label="Watch jabedzaman/python-scripts on GitHub"
+            >
+              Watch
+            </GitHubButton>
+          </div>
+          {/* <!-- Place this tag where you want the button to render. --> */}
+          <div className=" ">
+            <GitHubButton
+              href="https://github.com/jabedzaman/python-scripts/discussions"
+              data-color-scheme="no-preference: light; light: light; dark: light;"
+              data-icon="octicon-comment-discussion"
+              data-size="large"
+              aria-label="Discuss jabedzaman/python-scripts on GitHub"
+            >
+              Discuss
+            </GitHubButton>
+          </div>
+        </div>
       </motion.div>
     </div>
   );
